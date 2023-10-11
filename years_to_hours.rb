5e597af8b08eebc4511edaded276686421fb3cd9
muginga72 <lmj.muginga@gmail.com>
2023-10-11 19:03:49 -0400

    Week 2: Assignment introduction to SQL and Ruby Basic

---
+++ years_to_hours.rb
@@ -0,0 +1,20 @@
+# Defining the method
+def years_to_hours
+  puts "Enter number of years: "
+  years = gets.chomp.to_i # return string and convert it to integer
+  hours = years * 365 * 24
+  puts "#{years} years is #{hours} hours."
+
+  puts "Enter the number of decades: "
+  decades = gets.chomp.to_i # return string and convert it to integer
+  minutes = decades * 10 * 365 * 24 * 60
+  puts "#{decades} decades is #{minutes} minutes."
+
+  puts "Enter your age: "
+  age = gets.chomp.to_i # return string and convert it to integer
+  seconds = age * 365 * 60 * 60
+  puts "You are #{seconds} seconds old."
+end
+
+# Calling the method
+years_to_hours
